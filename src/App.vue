bfdddc04d7e20c8782d387a51e3640afa7596f1e
ADAM <kd20220905@gmailc.com>
2022-12-29 12:01:47 +0800

    fix bg pic url v2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="bg-[url('./src/assets/bg.png')] min-h-screen pb-[5%] pt-[1%]">
+  <div class="bg-[url('./bg.png')] min-h-screen pb-[5%] pt-[1%]">
     <RouterView />
   </div>
 </template>
