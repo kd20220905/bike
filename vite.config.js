8a91081d7fbe7e1b41c90901b51c91cdaa9aef84
ADAM <kd20220905@gmailc.com>
2024-03-06 22:29:33 +0800

    更新打包點

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bike/dist/",
+  base: "/testXH/",
   plugins: [vue()],
   resolve: {
     alias: {
